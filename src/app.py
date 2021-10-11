1dcd5a522a346a968e056ef05a16d1463d80b4d4
folknik <fadeev1087@gmail.com>
2021-10-11 23:21:33 +0300

    select last record by timestamp

--- src/app.py
+++ src/app.py
@@ -2,7 +2,11 @@ import os
 import random
 import logging
 import logging.handlers
+import pymongo
 from pymongo import MongoClient
+
+import pytz
+from datetime import datetime
 
 
 handler = logging.handlers.RotatingFileHandler(
@@ -26,21 +30,28 @@ def main():
     noise_records = db.noise
 
     i = 0
-    while i < 100:
+    while i < 10:
         adsb_record = {
-            'id': str(i + 1),
+            'uid': str(i + 1),
+            'time_stamp': datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime("%Y-%m-%d %H:%M:%S"),
             'value': random.randint(0, 100)
         }
         adsb_result = adsb_records.insert_one(adsb_record)
         logging.info(f"New adsb record insert to mongodb, id: {adsb_result.inserted_id}")
 
         noise_record = {
-            'id': str(i + 1),
+            'uid': str(i + 1),
+            'time_stamp': datetime.now(tz=pytz.timezone('Europe/Moscow')).strftime("%Y-%m-%d %H:%M:%S"),
             'value': random.randint(0, 1000)
         }
         noise_result = noise_records.insert_one(noise_record)
         logging.info(f"New noise record insert to mongodb, id: {noise_result.inserted_id}")
         i = i + 1
+
+    last_adsb_record = db.adsb.find().sort([("time_stamp", pymongo.DESCENDING)]).limit(1)[0]
+    last_noise_record = db.noise.find().sort([("time_stamp", pymongo.DESCENDING)]).limit(1)[0]
+    logging.info(f"Last adsb record:: {last_adsb_record}")
+    logging.info(f"Last noise record:: {last_noise_record}")
 
 
 if __name__ == "__main__":
